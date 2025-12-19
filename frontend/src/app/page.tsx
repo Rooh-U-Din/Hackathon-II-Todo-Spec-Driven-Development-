61d52280250eea90771bc5aceb0c1b0b962dab64
Rooh-U-Din <fidajokhio2@gmail.com>
2025-12-19 11:44:27 +0500

    first commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { isAuthenticated } from '@/lib/auth';
+import { isAuthenticated } from '../../lib/auth'; 
 
 export default function Home() {
   const router = useRouter();
